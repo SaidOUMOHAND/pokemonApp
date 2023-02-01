32ededf6b40adb7dde43f60ad97cb537ef32ac45
SOmohand <said.oumohand@hardis-group.com>
2023-02-01 12:05:32 +0100

    "Ajout des composants pour la navigation routing"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { POKEMONS } from './mock-pokemon-list';
 const routes: Routes = [];
+/**
+ * Ca permet de faire de la navigation entre les pages
+ */
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/list-pokemon/list-pokemon.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+/**
+ * Génération du composant ng generate component list-pokemon --inline-template=false
+ */
+@Component({
+  selector: 'app-list-pokemon',
+  templateUrl: './list-pokemon.component.html',
+  styles: [
+  ]
+})
+export class ListPokemonComponent {
+
+}
